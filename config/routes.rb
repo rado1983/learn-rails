a197c0e698db6ff1876521c2465fcd4fc637df54
Radoslaw Sieprawski <massoft@massoft.pl>
2014-05-03 17:05:44 +0000

    first dynamic home page in ruby on rails woow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearnRails::Application.routes.draw do
+  root to: 'visitors#new' #Any request to the application root (http://localhost:3000/) will be directed to the VisitorsController new action.
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,3 @@ LearnRails::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
