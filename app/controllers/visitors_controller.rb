874e97b467f170052c610ef1d0bbf3c4c39c55d9
Radoslaw Sieprawski <massoft@massoft.pl>
2014-05-04 14:04:42 +0000

    controller hidden code added as suggestion/advice for beginners

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,7 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+    #render 'visitors/new' #hidden code used by default, could be added for clarity for beginners
 	end
 
 end
