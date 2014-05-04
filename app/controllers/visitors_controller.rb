60980788b65bd7dfb7715fdc80efaa0817332608
Radoslaw Sieprawski <massoft@massoft.pl>
2014-05-04 16:05:04 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
     #render 'visitors/new' #hidden code used by default, could be added for clarity for beginners
+    flash.now[:notice] = 'Welcome Guys!'
+    flash.now[:alert] = 'My birthday is soon!'
 	end
 
 end
